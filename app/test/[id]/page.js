0257335c52eb69eec6081bdc80d2ab36c3fbca53
Fergus Leen <leenf@tcd.ie>
2024-07-09 17:47:52 +0200

    ISR test

--- app/test/[id]/page.js
+++ app/test/[id]/page.js
@@ -1,7 +1,21 @@
-export default function Home({params}) {
+import { createClient } from '../../../lib/supabase/client';
+
+export default async function Home({ params }) {
+  const supabase = createClient();
+  const { data, error } = await supabase.from('rikishi').select().limit(10);
+
+  console.log(data, error)
+
   return (
     <div>
       The id of this page is {params.id}
+
+      {
+        data?.map((rikishi) => (
+          <div key={rikishi.id}>
+            {rikishi.shikona}
+          </div>
+        ))}
     </div>
   );
 }
