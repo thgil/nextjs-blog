79d163cf97d9df3397eec646e3692fed79eb594a
Fergus Leen <leenf@tcd.ie>
2024-07-09 23:19:05 +0200

    Removes reach cache

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,10 +1,9 @@
-import { cache } from "react";
 import { createClient } from './supabase/client';
 
 
-export const getFriends = cache(async (id) => {
+export const getFriends = async (id) => {
   const supabase = createClient();
   const { data, error } = await supabase.from('rikishi').select().gt('id', id).limit(10);
 
   return data;
-});
+};
