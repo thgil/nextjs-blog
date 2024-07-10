70fee2bf01d512d73f83742f875cf10369419961
Fergus Leen <leenf@tcd.ie>
2024-07-10 09:19:01 +0200

    Adds unstable cache

--- src/app/test/[id]/page.js
+++ src/app/test/[id]/page.js
@@ -2,16 +2,6 @@ import { createClient } from '../../../lib/supabase/client';
 import Link from 'next/link';
 import { getFriends } from '../../../lib/db';
 
-
-
-export async function generateStaticParams() {
-  const ids = [1, 2, 3, 4, 5]
-  return ids.map((id) => ({
-    id: id.toString(),
-  }));
-}
-
-
 export default async function Home({ params }) {
   // get time of page load
   const start = new Date().getTime();
@@ -29,12 +19,12 @@ export default async function Home({ params }) {
     <div>
       The id of this page is {params.id} for {data?.shikona}
 
-      {
+      {/* {
         friends?.map((rikishi) => (
           <div key={rikishi.id}>
             <Link href={`/test/${rikishi.id}`}>{rikishi.shikona}</Link>
           </div>
-        ))}
+        ))} */}
       <p>Current time was {new Date(end).getHours()}:{new Date(end).getMinutes()}:{new Date(end).getSeconds()}:{new Date(end).getMilliseconds()}</p>
       <p>Page loaded in {end - start}ms</p>
     </div>
--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,13 @@
 import { createClient } from './supabase/client';
+import { unstable_cache } from 'next/cache';
+import { cache } from 'react';
 
 
-export const getFriends = async (id) => {
+export const getFriends = unstable_cache(async (id) => {
+  console.log('getFriends', id);
+  
   const supabase = createClient();
   const { data, error } = await supabase.from('rikishi').select().gt('id', id).limit(10);
 
   return data;
-};
+});
